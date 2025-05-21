0fb3cec13869be96b7e47d389bceaf0f7f3a8aa6
cwjcw <zhuce@cuixiaoyuan.cn>
2025-05-21 09:50:07 +0800

    调整了API KEY bug

--- wechat_pusher.py
+++ wechat_pusher.py
@@ -3,6 +3,7 @@ from project_config import FANGTANG_SENDKEY
 
 def push_to_wechat(title, link, summary):
     markdown_msg = f"### {title}\n\n{summary}\n\n👉 [阅读全文]({link})"
+    print(markdown_msg)
     url = f"https://sctapi.ftqq.com/{FANGTANG_SENDKEY}.send"
     data = {
         "title": title,
